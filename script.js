8b3c4931764e25baa8253b425a708ad902ed1507
Luis Fernando CS <fwzk777@gmail.com>
2024-02-14 21:11:12 -0400

    Update title and buttons text

--- script.js
+++ script.js
@@ -1,26 +1,42 @@
-$( document ).ready(function() {
-    
-    var envelope = $('#envelope');
-    var btn_open = $("#open");
-    var btn_reset = $("#reset");
-    
-    envelope.click( function() {
-        open();
-    });
-    btn_open.click( function() {
-        open();
-    });
-    btn_reset.click( function() {
-        close();
-    });
+$(document).ready(function () {
+  var envelope = $("#envelope");
+  var btn_open = $("#open");
+  var btn_reset = $("#reset");
 
-    function open() {
-        envelope.addClass("open")
-           .removeClass("close");
+  envelope.click(function () {
+    open();
+  });
+  btn_open.click(function () {
+    open();
+  });
+  btn_reset.click(function () {
+    close();
+  });
+
+  function open() {
+    envelope.addClass("open").removeClass("close");
+  }
+  function close() {
+    envelope.addClass("close").removeClass("open");
+  }
+});
+document.addEventListener("DOMContentLoaded", function () {
+  const heart = document.querySelector(".heart");
+  const message = document.getElementById("message");
+  const container = document.querySelector(".container");
+  const containerRect = container.getBoundingClientRect();
+
+  function checkIfOutOfViewport() {
+    const heartRect = heart.getBoundingClientRect();
+    if (
+      heartRect.right < containerRect.left ||
+      heartRect.left > containerRect.right ||
+      heartRect.bottom < containerRect.top ||
+      heartRect.top > containerRect.bottom
+    ) {
+      message.textContent = "¡Eres una crack!";
     }
-    function close() {
-        envelope.addClass("close")
-           .removeClass("open");
-    }
-   
+  }
+
+  setInterval(checkIfOutOfViewport, 100);
 });
